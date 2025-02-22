469756e88d2c89ae8f6ed13b9fdd0ef33e9130cc
Ndabenhle Nqubeko Zamisa <140793250+Ndabe-Zamisa@users.noreply.github.com>
2025-02-22 13:38:06 +0200

    Add files via upload

---
+++ #This a Product name creator.py
@@ -0,0 +1,5 @@
+#This a Product name creator
+brand_name = input("Enter the brand name:")
+model_name = input("Enter the model nme:")
+Product_name = brand_name + " " + model_name
+print(Product_name)
